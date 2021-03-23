2914c96a3159e69fa5094424f7b4b09b670faadd
edgar193241146 <80978764+edgar193241146@users.noreply.github.com>
2021-03-23 13:19:20 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -7,6 +7,6 @@ firebase.initializeApp({
     projectId: "autentificacion43eaot",
     storageBucket: "autentificacion43eaot.appspot.com",
     messagingSenderId: "726177240649",
-    appId: "1:726177240649:web:b1ca4c3cd5bcf08b251198",
-    measurementId: "G-0G523KMV7R"
+    appId: "1:726177240649:web:4ca8a75933f94f83251198",
+    measurementId: "G-6QD7H7DESV"
     });
